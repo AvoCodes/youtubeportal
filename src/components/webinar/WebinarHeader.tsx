fb5941b390d2363d09496f80f4817821d7cf8b3b
AvoCodes <andreiostrooukhov@gmail.com>
2025-03-26 18:47:09 +0400

    Update WebinarHeader.tsx

--- src/components/webinar/WebinarHeader.tsx
+++ src/components/webinar/WebinarHeader.tsx
@@ -3,11 +3,16 @@ import React from 'react';
 
 const WebinarHeader: React.FC = () => {
   return (
-    <div className="mb-8">
-      <h1 className="text-xl md:text-3xl font-medium text-slate-900 mb-2">
-        The YouTube Portal: Make $57/Hr Without Showing Your Face.
+    <div className="mb-8 text-center md:text-left">
+      <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-white mb-3 tracking-tight">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-rose-500">
+          The YouTube Portal:
+        </span>{" "}
+        Make $57/Hr Without Showing Your Face.
       </h1>
-      <p className="text-base md:text-xl text-slate-600">With Daniel Bitton, Made First Million At 17.</p>
+      <p className="text-base md:text-xl text-slate-300 tracking-wide">
+        With Daniel Bitton, Made First Million At 17.
+      </p>
     </div>
   );
 };
